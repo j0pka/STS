6738ded69d7c8f436b49521e5e51e2130fbd01aa
j0pka <jo.pka@icloud.com>
2024-12-18 23:38:54 +0500

    тест

---
+++ STSerApp1/STSerApp/Models/EventModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STSerApp.Models
+{
+    public class EventModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; } 
+    }
+}
+
---
+++ STSerApp1/STSerApp/Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STSerApp.Models
+{
+    public class TaskModel
+    {
+        public string Name { get; set; } // Название задачи
+        public string Description { get; set; } // Описание задачи
+        public DateTime StartDate { get; set; } // Дата начала задачи
+    }
+}
